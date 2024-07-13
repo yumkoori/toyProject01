0c6684514f9dd3d293624b0850840bc9bd45f134
yumi <yumkoori@gmail.com>
2024-07-13 23:07:12 +0900

    Feat : response를 위한 dto와 http 응답 메세지 클래스 작성
    - ResultDto: 응답 httpp 메세지에 반환해줄 정보들을 작성함
    - ResponseMemberDto : 회원가입 성공시 닉네임을 반환하도록 작성함

--- src/main/java/toyProject/toyProject01/member/adapter/in/web/ResponseMemberDto.java
+++ src/main/java/toyProject/toyProject01/member/adapter/in/web/ResponseMemberDto.java
@@ -1,4 +1,15 @@
 package toyProject.toyProject01.member.adapter.in.web;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+
 public class ResponseMemberDto {
+
+    private final String nickName;
+
+    static ResponseMemberDto mapToResponseDto(String nickName) {
+        return new ResponseMemberDto(nickName);
+    }
 }
