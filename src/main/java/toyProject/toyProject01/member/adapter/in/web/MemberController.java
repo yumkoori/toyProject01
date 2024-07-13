05f08f0212172b8f3e40beb9b8ce2b472fff16bc
yumi <yumkoori@gmail.com>
2024-07-13 23:17:07 +0900

    Form Controller와 RestController를 따로 분리함
    - 한쪽으로 몰아넣으니까 @Controller 따로, @RestController 따로 작성해줘야해서 분리하는게 나을것 같아서 분리함
    - RestController는 test 완료

--- src/main/java/toyProject/toyProject01/member/adapter/in/web/MemberController.java
+++ src/main/java/toyProject/toyProject01/member/adapter/in/web/MemberController.java
@@ -1,19 +1,28 @@
 package toyProject.toyProject01.member.adapter.in.web;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import toyProject.toyProject01.member.application.port.in.MemberJoinUseCase;
+import toyProject.toyProject01.member.application.port.in.command.JoinCommand;
+import toyProject.toyProject01.member.common.ResultDto;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
 
-    @GetMapping(value = "/members/join")
+    @GetMapping(value = "/members/join")            //uri 이름 수정예정(자원만담아야함)
     public String MemberJoinForm (Model model) {
         model.addAttribute("requestJoinDto", new RequestJoinDto());
         return "members/joinMemberForm";
     }
-
 
 }
