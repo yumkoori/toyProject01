174ef4a726e699f5559da91a79c0eb6987cbb6fe
yumi <yumkoori@gmail.com>
2024-07-14 16:26:40 +0900

    SpringjpaData에 update관련 쿼리 추가

--- src/main/java/toyProject/toyProject01/member/adapter/out/persistence/SpringDataMemberRepository.java
+++ src/main/java/toyProject/toyProject01/member/adapter/out/persistence/SpringDataMemberRepository.java
@@ -1,6 +1,7 @@
 package toyProject.toyProject01.member.adapter.out.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -12,4 +13,7 @@ public interface SpringDataMemberRepository extends JpaRepository <MemberJpaEnti
     @Query("select m from MemberJpaEntity m where m.memberId = :memberId")
     MemberJpaEntity findByMemberId(@Param("memberId") String memberId);
 
+    @Modifying
+    @Query("update MemberJpaEntity m SET m.nickName = :nickName WHERE m.memberId = :memberId")
+    void updateNickName(@Param("nickName") String nickName, @Param("memberId") String memberId);
 }
