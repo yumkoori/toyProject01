24184396f91f588146c56ba7b888757ef1492d79
yumi <yumkoori@gmail.com>
2024-07-17 19:50:25 +0900

    Refator : ErrorCode에서 code필드 구조 재정의
    - "M001"과 같은 code 데이터는 현재 의미가 없고, 필요 용도가 아니기때문에 삭제함
    
    - 기존 code와 status가 의미적으로 헷갈릴 수 있기 때문에 Integer 타입을 code 필드로 재정의함

--- src/main/java/toyProject/toyProject01/member/common/ToyProjectErrorCode.java
+++ src/main/java/toyProject/toyProject01/member/common/ToyProjectErrorCode.java
@@ -8,19 +8,17 @@ public enum ToyProjectErrorCode {
     //10xx Common
 
     //11xx Member
-    EMAIL_DUPLICATION(1100, "M001", "이미 존재하는 이메일"),
-    EMAIL_NOT_EXISTENCE(1101, "MOO2", "존재하지 않는 이메일"),
-    LOGIN_PW_WRONG(1003, "MOO3", "일치하지 않는 비밀번호"),
+    EMAIL_DUPLICATION(1100,"이미 존재하는 이메일"),
+    EMAIL_NOT_EXISTENCE(1101,  "존재하지 않는 이메일"),
+    LOGIN_PW_WRONG(1102,  "일치하지 않는 비밀번호"),
     ;
 
     //12xx Board
 
-    private final Integer status;
-    private final String code;
+    private final Integer code;
     private final String message;
 
-    ToyProjectErrorCode(Integer status, String code, String message) {
-        this.status = status;
+    ToyProjectErrorCode(Integer code, String message) {
         this.code = code;
         this.message = message;
     }
