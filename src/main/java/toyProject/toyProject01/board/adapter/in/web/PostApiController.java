ec0464369d89b3ca20d051503375900bd8f4d1c0
yumi <yumkoori@gmail.com>
2024-07-20 01:27:17 +0900

    Feat : 모든 글 조회 기능을 위한 웹 어댑터 구현
    - 글 조회를 위한 'loadPostUseCase' 의존성 주입 추가
    - 단순 조회이므로 HTTP 메서드 타입은 Get으로 지정
    - 모든글 조회 반환을 위해 ResultDto의 제네릭 타입으로 List<ResponseDto> 지정

--- src/main/java/toyProject/toyProject01/board/adapter/in/web/PostApiController.java
+++ src/main/java/toyProject/toyProject01/board/adapter/in/web/PostApiController.java
@@ -4,16 +4,22 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import toyProject.toyProject01.board.adapter.in.web.dto.ResisterPostDto;
 import toyProject.toyProject01.board.adapter.in.web.dto.ResponsePostDto;
+import toyProject.toyProject01.board.application.port.in.LoadPostUseCase;
 import toyProject.toyProject01.board.application.port.in.ResisterPostUseCase;
 import toyProject.toyProject01.board.application.port.in.command.ResisterPostCommand;
 import toyProject.toyProject01.board.domain.Post;
 import toyProject.toyProject01.common.ResultDto;
 import toyProject.toyProject01.common.WebAdapter;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @WebAdapter
@@ -22,6 +28,7 @@ import toyProject.toyProject01.common.WebAdapter;
 public class PostApiController {
 
     private final ResisterPostUseCase resisterPostUseCase;
+    private final LoadPostUseCase loadPostUseCase;
 
     @PostMapping(value = "/posts")
     public ResponseEntity<ResultDto<ResponsePostDto>> resisterPost(@RequestBody ResisterPostDto request) {
@@ -42,4 +49,20 @@ public class PostApiController {
 
         return new ResponseEntity<>(result, HttpStatus.CREATED);
     }
+
+    //모든 게시물 조회
+    @GetMapping("/posts")
+    public ResponseEntity<ResultDto<List<ResponsePostDto>>> findAllPosts() {
+
+        //모든 게시물 리스트로 조회
+        //응답Dto타입의 리스트로 변환
+        List<ResponsePostDto> responseDto = loadPostUseCase.findPostAll().stream()
+                .map(ResponseMapper::mapToPostDomain)
+                .collect(Collectors.toList());
+
+        ResultDto<List<ResponsePostDto>> result = new ResultDto<>(200, "게시물 조회 완료", responseDto);
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
 }
